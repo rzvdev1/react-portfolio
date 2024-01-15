c47f7a31d1503dd463d062cff2eff88033d69d3f
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2024-01-15 03:33:59 -0700

    :construction: Portfolio in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Footer from './Footer';
 
 export default function App() {
   return (
     <>
       <Header title='Hello World!' />
+      <Footer />
     </>
   );
 }
